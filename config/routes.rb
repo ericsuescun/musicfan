1a620c343ad11df45c9c54fe54bed2ecc24f410c
Eric Suescun <edsuescun@gmail.com>
2019-03-13 09:59:03 -0500

    Se hace un cambio en los resources para usar la convencion artistS y albumS, pero no funciona porque ya genere los controladores en singular, así que un paso atrás

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-	resources :artists do
-		resources :albums
+	resources :artist do
+		resources :album
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
