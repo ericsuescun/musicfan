166eb520f129e24c8a8224d7984451ce553322fb
Eric Suescun <edsuescun@gmail.com>
2019-03-13 09:32:00 -0500

    Se crea el Model para Album

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_030113) do
+ActiveRecord::Schema.define(version: 2019_03_13_143107) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.date "released_at"
+    t.integer "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_albums_on_artist_id"
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
